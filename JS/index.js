a40b0171d7d24464cf1217586acb160a157206a0
qalejxndrop <76859976+TECNIMUX-DEV@users.noreply.github.com>
2024-05-19 05:14:40 -0500

    Scroll

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
  // document.addEventListener('DOMContentLoaded', Function ()}): 
- 
+//  Cambio de color bottom header 
  window.addEventListener('scroll', function() {
     var header = document.getElementById('header-01');
     var scrollPosition = window.scrollY;
@@ -16,8 +16,25 @@ if (typeof window !== 'undefined') {
   value = window.localStorage.getItem('key')
 }
 
+// Scroll animation
+// Scrolling down
+// Scrolling up
+document.addEventListener('DOMContentLoaded', () => {
+    let sections = document.querySelectorAll('section');
 
-
-
-
-
+    window.addEventListener('scroll', () => {
+        sections.forEach((section, index) => {
+            if(isElementInView(section, 0.5)) {
+            section.classList.remove('section-hidden');
+        } else {
+            section.classList.add('section-hidden');
+        }
+        });
+    });
+    
+function  isElementInView(el, percentVisible) {
+let rect = el.getBoundingClientRect();
+let windowHeight = window.innerHeight || document.documentElement,clientHeight;
+return !(rect.bottom < 0 || rect.top - windowHeight >= percentVisible * el.clientHeight);
+} 
+});
