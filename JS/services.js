c45b73defb27b743c438b8a661004d917e670aee
TECNIMUX <76859976+TECNIMUX-DEV@users.noreply.github.com>
2025-02-26 23:53:40 -0500

    Add new menu & header

--- JS/services.js
+++ JS/services.js
@@ -1,6 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 // Menu
 const menuMain = document.getElementById('menu-main');
-const btnMenu = document.getElementById('btn-menu');
+const btnMenu = document.getElementById('burger');
 
 let stateToggle = true;
 btnMenu.addEventListener('click', () => {
@@ -9,7 +11,7 @@ btnMenu.addEventListener('click', () => {
         btnMenu.style.position = "fixed";
     } else {
         menuMain.style.display = "none";
-        btnMenu.style.position = "static";
+        btnMenu.style.position = "fixed";
     }
 stateToggle =! stateToggle;
 })
@@ -28,3 +30,31 @@ somosTX.style.display = "none";
 who.addEventListener('click', () => {
     somosTX.style.display = "flex";
     })
+
+
+// Add header color when scrolling
+let lastScrollY = window.scrollY;
+const header01 = document.querySelector('header');
+
+ window.addEventListener('scroll', () => {
+     if (window.scrollY > lastScrollY) {
+         header01.classList.add("header-hidden")
+     } else if (lastScrollY <= 100){
+        header01.classList.remove("header-hidden")
+         }
+         lastScrollY = window.scrollY;
+         });
+
+});
+
+
+// Button back to top
+let backToTop = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 150 ) {
+     backToTop.style.display= "block";
+  } else {
+     backToTop.style.display= "none";
+      }
+      });
